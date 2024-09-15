53f472bdbeb434dde2f00eeb43ad0b62a05cbe93
muj-i <dev.muj.i@icloud.com>
2024-09-15 11:05:29 +0600

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:aw_noti/firebase_options.dart';
 import 'package:aw_noti/services/awesome_noti.dart';
+import 'package:aw_noti/services/awsm_fcm_services.dart';
 import 'package:firebase_core/firebase_core.dart';
 // import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
@@ -13,14 +15,9 @@ import './the_app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    name: 'local-noti',
-    options: const FirebaseOptions(
-      apiKey: 'your-api-key',
-      appId: '1:408136830883:android:4ab0d0960c2164d6239416',
-      messagingSenderId: 'your-sender-id',
-      projectId: 'local-noti-95efb',
-    ),
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  await AwsmFcmServices.initializeRemoteNotifications(debug: false);
   await AwesomeNotificationController.init();
   runApp(const TheApp());
 }
--- lib/services/awsm_fcm_services.dart
+++ lib/services/awsm_fcm_services.dart
@@ -8,47 +8,58 @@ import 'package:firebase_core/firebase_core.dart';
 class AwsmFcmServices {
   static Future<void> initializeRemoteNotifications(
       {required bool debug}) async {
-    await Firebase.initializeApp();
+    //   await Firebase.initializeApp(
+    // name: 'local-noti',
+    // options: const FirebaseOptions(
+    //   apiKey: 'AIzaSyAVxfmF7IdG8fPu0oC6id9_fkqUbCQe4pw',
+    //   appId: '1:408136830883:android:4ab0d0960c2164d6239416',
+    //   messagingSenderId: '408136830883',
+    //   projectId: 'local-noti-95efb',
+    // ),
+  // );
     await AwesomeNotificationsFcm().initialize(
         onFcmSilentDataHandle: mySilentDataHandle,
         onFcmTokenHandle: myFcmTokenHandle,
         onNativeTokenHandle: myNativeTokenHandle,
         debug: debug);
+    final tok = await AwesomeNotificationsFcm().requestFirebaseAppToken();
+    log("tok: $tok");
+    AwesomeNotificationsFcm().subscribeToTopic('123xxx');
+  }
+}
+
+///  *********************************************
+///     REMOTE NOTIFICATION EVENTS
+///  *********************************************
+
+/// Use this method to execute on background when a silent data arrives
+/// (even while terminated)
+@pragma("vm:entry-point")
+Future<void> mySilentDataHandle(FcmSilentData silentData) async {
+  log('"SilentData": ${silentData.toString()}');
+
+  if (silentData.createdLifeCycle != NotificationLifeCycle.Foreground) {
+    log("bg");
+  } else {
+    log("FOREGROUND");
   }
 
-  ///  *********************************************
-  ///     REMOTE NOTIFICATION EVENTS
-  ///  *********************************************
+  log("starting long task");
+  await Future.delayed(const Duration(seconds: 4));
+  const url = ("http://google.com");
+  final re = await Dio().get(url);
+  log(re.data);
+  log("long task done");
+}
 
-  /// Use this method to execute on background when a silent data arrives
-  /// (even while terminated)
-  @pragma("vm:entry-point")
-  static Future<void> mySilentDataHandle(FcmSilentData silentData) async {
-    log('"SilentData": ${silentData.toString()}');
+/// Use this method to detect when a new fcm token is received
+@pragma("vm:entry-point")
+Future<void> myFcmTokenHandle(String token) async {
+  log('FCM Token:"$token"');
+}
 
-    if (silentData.createdLifeCycle != NotificationLifeCycle.Foreground) {
-      log("bg");
-    } else {
-      log("FOREGROUND");
-    }
-
-    log("starting long task");
-    await Future.delayed(const Duration(seconds: 4));
-    const url = ("http://google.com");
-    final re = await Dio().get(url);
-    log(re.data);
-    log("long task done");
-  }
-
-  /// Use this method to detect when a new fcm token is received
-  @pragma("vm:entry-point")
-  static Future<void> myFcmTokenHandle(String token) async {
-    log('FCM Token:"$token"');
-  }
-
-  /// Use this method to detect when a new native token is received
-  @pragma("vm:entry-point")
-  static Future<void> myNativeTokenHandle(String token) async {
-    log('Native Token:"$token"');
-  }
+/// Use this method to detect when a new native token is received
+@pragma("vm:entry-point")
+Future<void> myNativeTokenHandle(String token) async {
+  log('Native Token:"$token"');
 }
